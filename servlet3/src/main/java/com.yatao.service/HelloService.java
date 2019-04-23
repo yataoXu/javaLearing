c906d6f529d7d4a8b319c5d4c2e623df51834845
Evan <xuyt1992@163.com>
2019-04-23 21:09:27 +0800

    add : servlet 3

---
+++ servlet3/src/main/java/com.yatao.service/HelloService.java
@@ -0,0 +1,4 @@
+package com.yatao.service;
+
+public class HelloService {
+}
---
+++ servlet3/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // TODO Auto-generated method stub
+        //super.doGet(req, resp);
+        System.out.println(Thread.currentThread() + " start...");
+        try {
+            sayHello();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        resp.getWriter().write("hello...");
+        System.out.println(Thread.currentThread() + " end...");
+    }
+
+
+    private void sayHello() throws InterruptedException {
+
+        System.out.println(Thread.currentThread() + " processing...");
+        Thread.sleep(3000);
+
+    }
+}
