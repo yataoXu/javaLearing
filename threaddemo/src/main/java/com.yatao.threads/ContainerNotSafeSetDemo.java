1a35d37796efaa5691574c0fef6c0109184e1fbd
Evan <xuyt1992@163.com>
2019-04-28 03:04:52 +0800

    add : gitignore

--- threaddemo/src/main/java/com.yatao.threads/ContainerNotSafeSetDemo.java
+++ threaddemo/src/main/java/com.yatao.threads/ContainerNotSafeSetDemo.java
@@ -2,19 +2,20 @@ package com.yatao.threads;
 
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * 集合类不安全的问题
  * ArrayList 是线程不安全的
  * why add方法是没有加锁的
  */
-public class ContainerNotSafeDemo {
+public class ContainerNotSafeSetDemo {
     public static void main(String[] args) {
-        List list = new CopyOnWriteArrayList();
+        Set set = new CopyOnWriteArraySet();//Collections.synchronizedSet(new HashSet<>());//new HashSet<>();
         for (int i = 1; i <= 30; i++) {
             new Thread(() -> {
-                list.add(UUID.randomUUID().toString().subSequence(0, 6));
-                System.out.println(Thread.currentThread().getName()+"-----"+list);
+                set.add(UUID.randomUUID().toString().subSequence(0, 6));
+                System.out.println(Thread.currentThread().getName()+"-----"+set);
             },String.valueOf(i)).start();
         }
         //java.util.ConcurrentModificationException
@@ -23,10 +24,8 @@ public class ContainerNotSafeDemo {
          *      java.util.ConcurrentModificationException
          * 2 导致原因
          * 3 解决方案
-         *      1 使用Vector
-         *              add()加锁啦，数据一致性可以得到保证，但是并发性大大降低
-         *      2 Collections.synchronizedList(new ArrayList<>());
-         *      3 使用 java.util.concurrent.CopyOnWriteArrayList
+         *      1 Collections.synchronizedSet(new HashSet<>());
+         *      2 使用 java.util.concurrent.CopyOnWriteArraySet;
          * 4 优化建议
          */
     }
