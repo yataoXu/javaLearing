450baea90541baa40560fe7a843e9449f7f1dabf
Evan <xuyt1992@163.com>
2019-05-02 22:14:34 +0800

    add : gc

---
+++ threaddemo/src/main/java/com/yatao/test/Son.java
@@ -0,0 +1,35 @@
+package com.yatao.test;
+
+public class Son extends Father {
+    private int i = test();
+    private static int j = method();
+
+    static {
+        System.out.print("(6)");
+    }
+
+    {
+        System.out.print("(7)");
+    }
+
+    Son() {
+        System.out.print("(8)");
+    }
+
+    public int test() {
+        System.out.print("(9)");
+        return 1;
+    }
+
+    public static int method() {
+        System.out.print("(10)");
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Son s1 = new Son();
+        System.out.println();
+        System.out.println("=============");
+        Son s2 = new Son();
+    }
+}
